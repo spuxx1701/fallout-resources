21854841af9992708eac7bc94037018609e5fa3b
Spuxx <ameisenfutter@outlook.com>
2022-12-01 16:10:48 +0100

    add preact and mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+import preact from "@astrojs/preact";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://spuxx1701.github.io",
   base: "/fallout-resources",
   trailingSlash: "always",
+  integrations: [mdx(), preact({ compat: true })],
 });
