9c3997c1997adf5b802d727b7ade52387058a01e
Spuxx <ameisenfutter@outlook.com>
2022-12-02 11:51:58 +0100

    add fake input field to allow toggling virtual keyboard on mobile/tablet

--- src/components/terminal-input/terminal-input.tsx
+++ src/components/terminal-input/terminal-input.tsx
@@ -16,28 +16,31 @@ export interface Props {
 export default function TerminalInput<Props>(props: Props) {
   const [value, setValue] = useState("");
   const [log, setLog] = useState([] as string[]);
+  const [showKeyboardToggle, setShowKeyboardToggle] = useState(false);
 
-  const handleKeyDown = (event: KeyboardEvent) => {
-    const { key } = event;
-    if (!key || key.length < 1) return;
-    switch (key) {
-      case "Enter":
-        event.preventDefault();
-        submit();
-        setValue("");
-        break;
-      case "Backspace":
-        event.preventDefault();
-        setValue(`${value.slice(0, -1)}`);
-        break;
-      default:
-        if (key.length > 1) return;
-        setValue(`${value}${key}`);
-    }
-  };
+  const handleKeyDown = useCallback(
+    (event: KeyboardEvent) => {
+      const { key } = event;
+      if (!key || key.length < 1) return;
+      switch (key) {
+        case "Enter":
+          event.preventDefault();
+          submit();
+          setValue("");
+          break;
+        case "Backspace":
+          event.preventDefault();
+          setValue(`${value.slice(0, -1)}`);
+          break;
+        default:
+          if (key.length > 1) return;
+          setValue(`${value}${key}`);
+      }
+    },
+    [value]
+  );
 
   const submit = () => {
-    console.log(props.successKeywords);
     if (props.commands?.length > 0) {
       for (const command of props.commands) {
         if (!Array.isArray(command.inputs)) command.inputs = [command.inputs];
@@ -58,6 +61,10 @@ export default function TerminalInput<Props>(props: Props) {
     setLog([...log, `> ${value}`]);
   };
 
+  const handleShowKeyboard = () => {
+    document.getElementById("terminal-input-field-hiden")?.focus();
+  };
+
   useEffect(() => {
     // Subscribe to window's keydown event to receive all key down events and
     // add them to the value
@@ -67,14 +74,31 @@ export default function TerminalInput<Props>(props: Props) {
     };
   }, [handleKeyDown]);
 
+  useEffect(() => {
+    setShowKeyboardToggle(
+      (window.innerWidth <= 768 && window.innerHeight <= 1024) ||
+        (window.innerWidth <= 1024 && window.innerHeight <= 768)
+    );
+  }, []);
+
   return (
     <div id="terminal-input">
       {log.map((str) => (
         <p>{str}</p>
       ))}
       <div id="terminal-input-field">
+        <input id="terminal-input-field-hiden" value={value} />
         <p id="terminal-input-text">{"> " + value}</p>
         <div id="terminal-input-cursor"></div>
+        {showKeyboardToggle && (
+          <button
+            class="terminal-keyboard-button"
+            type="button"
+            onClick={handleShowKeyboard}
+          >
+            <i class="fa-solid fa-keyboard"></i>
+          </button>
+        )}
       </div>
     </div>
   );
